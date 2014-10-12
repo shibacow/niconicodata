44a19ea01dc6b927cc24ef2eb5ad5f8eb6843036
Ubuntu <ubuntu@ip-10-156-234-183.ap-northeast-1.compute.internal>
2014-10-12 17:31:06 +0900

    ch data

--- flask_test.py
+++ flask_test.py
@@ -3,10 +3,32 @@
 
 from flask import Flask
 app = Flask(__name__)
+from flask import render_template
 
 @app.route("/")
-def hello():
-    return "Hello World!"
+def index():
+    return 'Index Page'
+
+@app.route("/hello")
+@app.route("/hello/<name>")
+def hello(name=None):
+    return render_template('hello.html',name=name)
+
+@app.route('/user/<username>')
+def show_user_profile(username):
+    return "User {}".format(username)
+
+@app.route('/post/<int:post_id>')
+def show_post(post_id):
+    return "Post %d" % post_id
+@app.route('/projects/')
+def projecsts():
+    return 'The Project Page'
+
+@app.route('/about')
+def about():
+    return 'the about page'
 
 if __name__ == "__main__":
-    app.run()
+    app.debug=True
+    app.run(host='0.0.0.0')
