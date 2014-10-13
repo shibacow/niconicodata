5a14430f1677d16f653664c6e493e4c26074e102
shibacow <shibacow@gmail.com>
2014-10-13 14:04:03 +0000

    add submit

--- flask_test.py
+++ flask_test.py
@@ -1,33 +1,43 @@
 #!/usr/bin/python
 # -*- coding:utf-8 -*-
 
-from flask import Flask
-app = Flask(__name__)
+
+from flask import Flask,redirect,flash
+from flask import current_app
 from flask import render_template
+from flaskext.wtf import Form
+from wtforms import StringField,IntegerField,TextAreaField
+from wtforms.validators import Required,NumberRange
+
+app = Flask(__name__)
+app.config['SECRET_KEY']='guest'
+app.config['CSRF_ENABLED']=True
+with app.app_context():
+    app.logger.debug(current_app.name)
+
+class MyForm(Form):
+    name = StringField('name',validators=[Required()])
+    number= IntegerField('number', validators=[NumberRange(min=0, max=10)])
+    queryarea=TextAreaField('query')
 
 @app.route("/")
 def index():
     return 'Index Page'
 
-@app.route("/hello")
-@app.route("/hello/<name>")
-def hello(name=None):
-    return render_template('hello.html',name=name)
+@app.route('/submit',methods=('GET','POST'))
+def submit():
+    form=MyForm()
+    if form.validate_on_submit():
+        #app.logger.debug(dir(form))
+        msg="name={} number={} query={}".format(form.name.data,form.number.data,form.queryarea.data)
+        app.logger.debug(msg)
+        return redirect('/sucess')
+    flash('fuga')
+    return render_template('submit.html',form=form)
 
-@app.route('/user/<username>')
-def show_user_profile(username):
-    return "User {}".format(username)
-
-@app.route('/post/<int:post_id>')
-def show_post(post_id):
-    return "Post %d" % post_id
-@app.route('/projects/')
-def projecsts():
-    return 'The Project Page'
-
-@app.route('/about')
-def about():
-    return 'the about page'
+@app.route('/sucess')
+def sucess():
+    return 'sucess'
 
 if __name__ == "__main__":
     app.debug=True
