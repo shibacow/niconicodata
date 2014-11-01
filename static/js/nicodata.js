e584e2665a2041edfffbf6a9a642d05502909dc6
shibacow <shibacow@gmail.com>
2014-11-01 05:59:33 +0000

    progress .

--- static/js/nicodata.js
+++ static/js/nicodata.js
@@ -2,32 +2,34 @@ function loaded_and_init(){
     $('#execute_button').click(execute_query);
 }
 function show_data_table(){
+    var data=[
+	['site_name','URL','Type','Date'],
+	["Sitepoint","http://sitepoint.com","Blog","2013-10-15 10:30:00"],
+	["Flippa","http://flippa.com","Marketplace","null"],
+	["99designs","http://99designs.com","Marketplace","null"],
+	["Learnable","http://learnable.com","Online courses","null"],
+	["Rubysource","http://rubysource.com","Blog","2013-01-10 12:00:00"]
+    ]
+    
+
     $("table#queryresulttable_inner").dataTable({
-	"aaData":[
-	    ["Sitepoint","http://sitepoint.com","Blog","2013-10-15 10:30:00"],
-	    ["Flippa","http://flippa.com","Marketplace","null"],
-	    ["99designs","http://99designs.com","Marketplace","null"],
-	    ["Learnable","http://learnable.com","Online courses","null"],
-	    ["Rubysource","http://rubysource.com","Blog","2013-01-10 12:00:00"]
-	],
-	"aoColumnDefs":[{
-            "sTitle":"Site name"
-	    , "aTargets": [ "site_name" ]
-	},{
-            "aTargets": [ 1 ]
-	    , "bSortable": false
-	    , "mRender": function ( url, type, full )  {
-		return  '<a href="'+url+'">' + url + '</a>';
+
+	"bPaginate": true,
+	"bLengthChange": false,
+	"bFilter": false,
+	"bSort": false,
+	"bInfo": true,
+	"bAutoWidth": false,
+	"aaData": data,
+	"aoColumnDefs":[
+	    {
+		"sTitle":"Site name"
+	    },{
+		"sTitle":"Uhogeuga",
+	    },{
+		"sTitle":"Hogefugahoge",
 	    }
-	},{
-            "aTargets":[ 3 ]
-	    , "sType": "date"
-	    , "mRender": function(date, type, full) {
-		return (full[2] == "Blog")
-                    ? new Date(date).toDateString()
-                    : "N/A" ;
-	    } 
-	}]
+	]
     });
 }
 function read_smid(){
