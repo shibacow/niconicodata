27a6e5e1e59191530c20db6fb295fc9c31c01c32
shibao koichiro <kshibao@aiming-inc.com>
2015-01-21 20:53:38 +0900

    init test case

---
+++ test.py
@@ -0,0 +1,7 @@
+from flast_test import app
+import unittest
+class MainTestCase(unittest.TestCase):
+    def test_index(self):
+        tester = app.test_client(self)
+        response = tester.get('/')
+        self.assertEqual(response.status_code, 200)
